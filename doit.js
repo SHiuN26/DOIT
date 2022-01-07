7fb7c8f23ff26fcf29a0e9a2161f2c8256cdaaea
SHiuN26 <justodme26@gmail.com>
2022-01-07 21:54:46 +0800

    1

--- doit.js
+++ doit.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
     
+    $(window).scroll(function(e){
+        // console.log($(window).scrollTop());
+        if ($(window).scrollTop()<=200)
+            $("#pic").hide();
+        else
+            $("#pic").fadeIn(2000);
+    });
+    
 });
+
