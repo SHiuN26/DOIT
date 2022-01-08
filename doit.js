ad2fa63b1999bf80e947f267bdd30050cafc6cb7
SHiuN26 <justodme26@gmail.com>
2022-01-09 04:07:16 +0800

    1

--- doit.js
+++ doit.js
@@ -1,3 +1,6 @@
+//CWB-FCE97AC9-3ED8-4811-BA23-2C1229A1EA5F
+// opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001
+
 $(document).ready(function () {
 
     $(window).scroll(function(e){
@@ -6,6 +9,14 @@ $(document).ready(function () {
             $("#pic").hide();
         else
             $("#pic").fadeIn(1000);
+    });
+    // const corsurl= "https://cors-anywhere.herokuapp.com/"
+    const apiurl ="https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-FCE97AC9-3ED8-4811-BA23-2C1229A1EA5F"
+    $.ajax({
+        url:apiurl,
+        method:"GET",
+        success:function(res){console.log(res)},
+        error:function(err){console.log(err)}
     });
 });
 
