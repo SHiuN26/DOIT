754fef2e8387dcb15f772c0d6f930053e1e1f930
SHiuN26 <justodme26@gmail.com>
2022-01-13 15:21:24 +0800

    1

--- doit.js
+++ doit.js
@@ -12,13 +12,23 @@ $(document).ready(function () {
         "url": apiurl, //要抓哪個地方的資料
         "method": "GET", //使用什麼方式抓
         "success": function(res){
-            // console.log(res);
-            // res.records.location.forEach(element => {
-            //     console.log(element.locationName);
-            // });
+            console.log(res),
             res.records.location.forEach(element => {
-                console.log(element.weatherElement.time);
+                var name =element.locationName//地區名稱
+                var one =element.weatherElement[0].time[0].parameter.parameterName//第一時段天氣情況
+                var oneMint =element.weatherElement[2].time[0].parameter.parameterName//第一時段低溫
+                var oneMaxt =element.weatherElement[4].time[0].parameter.parameterName//第一時段高溫
+
+                var two =element.weatherElement[0].time[1].parameter.parameterName //第二時段天氣情況
+                var twoMint =element.weatherElement[2].time[1].parameter.parameterName//第二時段低溫
+                var twoMaxt =element.weatherElement[4].time[1].parameter.parameterName//第二時段高溫
+
+                var three =element.weatherElement[0].time[2].parameter.parameterName//第三時段天氣情況
+                var threeMint =element.weatherElement[2].time[2].parameter.parameterName//第三時段低溫
+                var threeMaxt =element.weatherElement[4].time[2].parameter.parameterName//第三時段高溫
+                console.log(name,one,oneMint,oneMaxt,two,twoMint,twoMaxt,three,threeMint,threeMaxt); 
             });
+            
             // $("#dt").DataTable({
             //     "data":res,
             //     "columns":[{data:"res.records.location.lacotionName",title:"地區"},{data:"res.records.location.weatherElement.time.parameter.parameterName",title:"天氣狀況"}]
